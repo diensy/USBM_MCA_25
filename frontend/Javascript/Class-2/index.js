9a56851f403f3fdc46bdf81cba7d3e4c3f0e3129
Dinesh Kumar Sahoo <dineshsahoo702@gmail.com>
2024-10-07 11:13:06 +0530

    Operators opperations

---
+++ frontend/Javascript/Class-2/index.js
@@ -0,0 +1,34 @@
+// Undefined
+
+let data ;
+console.log(data);
+console.log(typeof data);
+
+//Number
+
+let num = 13;
+console.log(num);
+console.log("Type of num is :",typeof num);
+
+const pi = 3.14;
+
+console.log(pi);
+console.log("Type of PI is :",typeof pi);
+
+
+// Addititon 
+let a = 6;
+let b = 7;
+let sum = a +b;
+let min = a -b;
+let mul = a *b;
+let div = a /b;
+let mod = a %b;
+
+console.log("sum = ",sum);
+console.log("min = ",min);
+console.log("mul = ",mul);
+console.log("div = ",div);
+console.log("mod = ",mod);
+
+
