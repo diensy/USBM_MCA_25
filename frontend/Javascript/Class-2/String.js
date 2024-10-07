7f2802341f1f90f53e0f943e86c1dbd49ecd8013
Dinesh Kumar Sahoo <dineshsahoo702@gmail.com>
2024-10-07 11:54:30 +0530

    Biotics

---
+++ frontend/Javascript/Class-2/String.js
@@ -0,0 +1,15 @@
+// String
+
+let str = "Dinesh";
+console.log(str);
+
+let name = "Dinesh";
+let city = "DKL";
+let age = 21;
+let roll = 40;
+let college = "USBM";
+let Batch = "MCA";
+
+console.log(
+  `My name is ${name}, and I am From ${city}, and my age is ${age} , and I am currently study on ${Batch} in the ${college} college and my rollnumber is ${roll} `
+);
--- frontend/Javascript/Class-2/index.js
+++ frontend/Javascript/Class-2/index.js
@@ -1,6 +1,6 @@
 // Undefined
 
-let data ;
+let data;
 console.log(data);
 console.log(typeof data);
 
@@ -8,27 +8,52 @@ console.log(typeof data);
 
 let num = 13;
 console.log(num);
-console.log("Type of num is :",typeof num);
+console.log("Type of num is :", typeof num);
 
 const pi = 3.14;
 
 console.log(pi);
-console.log("Type of PI is :",typeof pi);
+console.log("Type of PI is :", typeof pi);
 
-
-// Addititon 
+// Addititon
 let a = 6;
 let b = 7;
-let sum = a +b;
-let min = a -b;
-let mul = a *b;
-let div = a /b;
-let mod = a %b;
+let sum = a + b;
+let min = a - b;
+let mul = a * b;
+let div = a / b;
+let mod = a % b;
 
-console.log("sum = ",sum);
-console.log("min = ",min);
-console.log("mul = ",mul);
-console.log("div = ",div);
-console.log("mod = ",mod);
+console.log("sum = ", sum);
+console.log("min = ", min);
+console.log("mul = ", mul);
+console.log("div = ", div);
+console.log("mod = ", mod);
 
+
+//bigInt
+let huge_no = 111111111111111111111111111111111111111111111111111111;
+console.log(huge_no);
+console.log("Type of huge_no is :", typeof huge_no);
+
+//Symbol 
+
+let sym1 = Symbol("sym1");
+let sym2 = Symbol("sym1");
+
+//NAN 
+
+let num5 = 0;
+let num6 = 0;
+
+console.log(num5/num6);
+
+// Boolean 
+
+let isAdult = true;
+let isSmart = false;
+
+console.log(typeof isAdult);
+console.log(isAdult);
+console.log(isSmart);
 
