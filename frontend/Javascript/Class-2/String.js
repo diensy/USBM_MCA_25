726f2b5077e0504017d394c23fe24359ebb7b8c5
Dinesh Kumar Sahoo <dineshsahoo702@gmail.com>
2024-10-07 22:53:08 +0530

    solve question

--- frontend/Javascript/Class-2/String.js
+++ frontend/Javascript/Class-2/String.js
@@ -13,3 +13,10 @@ let Batch = "MCA";
 console.log(
   `My name is ${name}, and I am From ${city}, and my age is ${age} , and I am currently study on ${Batch} in the ${college} college and my rollnumber is ${roll} `
 );
+
+console.log(name.length);
+
+let value = " Dinesh ";
+
+console.log(value.replace(" ",""));
+console.log(value.length);
